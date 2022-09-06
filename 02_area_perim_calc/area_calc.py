ca02508ef6a1bedb46e7cb645ba062ac96e62fe2
spatteghi <111542429+spatteghi@users.noreply.github.com>
2022-09-06 14:47:09 +1200

    Add files via upload

---
+++ 02_area_perim_calc/area_calc.py
@@ -0,0 +1,11 @@
+valid = False
+while not valid:
+
+    response = float(input("enter a number: "))
+
+    if response > 0:
+        valid = True
+
+    else:
+        print("please enter a number that is more than zero")
+        print()
